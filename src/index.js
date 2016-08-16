2694fdd8b3eebed7df1b9cb91cab42ca32349a5c
applqpak <applqpak@gmail.com>
2016-08-16 18:20:51 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var os = require('os');
+var dns = require('dns');
+var config = require('./resources/config.json');
 
 module.exports = {
+  create: function()
+  {
+
+  }
 };
